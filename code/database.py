48034f3e64463beead04c3f8bb0c7599f372d155
liodrong <932420839@qq.com>
2019-07-19 15:09:46 +0800

    2019-07-19:liodrong: formatted code

--- code/database.py
+++ code/database.py
@@ -1,7 +1,44 @@
 import pymysql
 
-DB=""
-CURSOR=""
+
+class MySQL_DB:
+	ip = ''
+	user = ''
+	pwd = ''
+	database = ''
+
+	DB = None
+	CURSOR = None
+
+	def __init__(self, ip, user, pwd, database):
+		self.ip = ip
+		self.user = user
+		self.pwd = pwd
+		self.database = database
+
+		self.DB = pymysql.connect(ip, user, pwd, database, charset='utf8')
+	
+	def execute(self, sql):
+		#print('todo save data to db')
+		try:
+			CURSOR = self.DB.cursor()
+			CURSOR.execute(sql)
+			data = CURSOR.fetchall()
+
+			self.DB.commit()
+
+			return data
+		except Exception as e:
+			print(e)
+			self.DB.rollback()
+
+	def __del__(self):
+		self.DB.close()
+
+
+"""
+DB=None
+CURSOR=None
 
 def execute(sql):
 	print('todo save data to db')
@@ -11,9 +48,25 @@ def execute(sql):
 
 def connect_db():
 	print('todo connect to database')
-	DB = pymysql.connect('localhost', 'drliu', 'drliu', 'on-sale-book', charset='utf8')
+	DB = pymysql.connect('localhost', 'drliu', 'drliu', 'on-sale_book', charset='utf8')
 	CURSOR = DB.cursor()
 
 def close_db():
 	DB.close()
 	print('todo close db connect')
+
+def execute2(sql):
+	print('todo save data to db')
+	DB = pymysql.connect('localhost', 'drliu', 'drliu', 'on-sale_book', charset='utf8')
+	CURSOR = DB.cursor()
+
+	print(type(DB))
+	print(type(CURSOR))
+
+	CURSOR.execute(sql)
+	data = CURSOR.fetchall()
+
+	DB.close()
+
+	return data
+	"""
--- code/main.py
+++ code/main.py
@@ -4,17 +4,43 @@ import datetime
 
 from os import path
 from os import makedirs
+import database as db
 
 catalog_url="http://apis.juhe.cn/goodbook/catalog?key={}&dtype=json"
 books_url="http://apis.juhe.cn/goodbook/query?catalog_id={}&pn={}&rn={}&dtype=json&key={}"
+
+#连接mysql数据库
+on_sale_book = db.MySQL_DB('localhost', 'drliu', 'drliu', 'on_sale_book')
 
 def get_catalog(url):
 	'''
 	获取图书分类
 	'''
-
 	print(url)
 
+	doc = requests.get(url)
+	catalog_json = json.loads(doc.text)
+	resultcode=catalog_json['resultcode']
+	reason=catalog_json['reason']
+	result=catalog_json['result']
+
+	if reason != "success":
+		print(str(reason) + str(result))
+		return {}
+
+	for catalog in result:
+		#print(catalog['id'], catalog['catalog'])
+		check_sql = "select * from book_catalog where catalog_id = %d" % (int(catalog['id']))
+		add_sql = "insert into book_catalog(catalog_id, catalog_name) value(%d, '%s')" % (int(catalog['id']), catalog['catalog'])
+
+		res = on_sale_book.execute(check_sql)
+
+		if len(res) == 0:
+			on_sale_book.execute(add_sql)
+
+	return result
+
+	"""
 	if path.isfile("../data/catalog.json"):
 		with open("../data/catalog.json", 'r') as file:
 			result_json = json.load(file)
@@ -23,18 +49,12 @@ def get_catalog(url):
 			file.close()
 			return result
 
-	doc = requests.get(url)
-	catalog_json = json.loads(doc.text)
-	resultcode=catalog_json['resultcode']
-	reason=catalog_json['reason']
-	result=catalog_json['result']
-	#for catalog in result:
-		#print(catalog['id'], catalog['catalog'])
 	if reason == "success":
 		with open('../data/catalog.json', 'w') as f:
 			json.dump(catalog_json, f)
 			f.close()
 		return result
+	"""
 
 def get_catalog_detail(detail_url, catalog):
 	'''
@@ -42,32 +62,58 @@ def get_catalog_detail(detail_url, catalog):
 	:param detail_url:
 	:return:
 	'''
-	print(detail_url)
+	#print(detail_url)
 
 	day_set=datetime.date.today()
 
-	if not path.exists("../data/{}".format(catalog)):
-		makedirs("../data/{}".format(catalog))
-	if not path.exists("../data/{}/{}".format(catalog, day_set)):
-		makedirs("../data/{}/{}".format(catalog, day_set))
-	if path.isfile("../data/{}/{}/detail.json".format(catalog, day_set)):
-		print("{}/{}/detail.json is exist !".format(catalog, day_set))
-		return 0
-
 	catalog_details = requests.get(detail_url)
 	catalog_details_json = json.loads(catalog_details.text)
+
 	resultcode = catalog_details_json['resultcode']
 	reason = catalog_details_json['reason']
+
 	if reason != 'Success':
 		return -1
-	with open("../data/{}/{}/detail.json".format(catalog, day_set), 'w') as f:
+
+	results = catalog_details_json['result']
+	data_detail = results['data']
+	print(data_detail)
+
+	for book in data_detail:
+		check_sql = "select * from book_info where title='%s' and day='%s'" %(book['title'], day_set)
+		add_sql = "insert into book_info(catalog_id, title, tags, sub1, sub2, img, online, reading, bytime, day) value(%d, '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', '%s')" % (
+			int(catalog['id']),
+			book['title'],
+			book['tags'],
+			book['sub1'],
+			book['sub2'],
+			book['img'],
+			book['online'],
+			int(book['reading'][:-3] if book['reading'][:-3] != '' else '0'),
+			book['bytime'],
+			day_set)
+
+		print(check_sql + "\n" + add_sql)
+		res = on_sale_book.execute(check_sql)
+		#print(res)
+		if len(res) == 0:
+			on_sale_book.execute(add_sql)
+
+	"""
+	if not path.exists("../data/{}".format(catalog['catalog'])):
+		makedirs("../data/{}".format(catalog['catalog']))
+	if not path.exists("../data/{}/{}".format(catalog['catalog'], day_set)):
+		makedirs("../data/{}/{}".format(catalog['catalog'], day_set))
+	if path.isfile("../data/{}/{}/detail.json".format(catalog['catalog'], day_set)):
+		print("{}/{}/detail.json is exist !".format(catalog['catalog'], day_set))
+		return 0
+	with open("../data/{}/{}/detail.json".format(catalog['catalog'], day_set), 'w') as f:
 		json.dump(catalog_details_json, f)
 		f.close()
-
+	"""
 	return 0
 
 if __name__ == '__main__':
-	#print(datetime.date.today())
 	f=open('./key.config', 'r')
 	key=f.readline(-1).split(':')[1]
 	f.close()
@@ -75,4 +121,4 @@ if __name__ == '__main__':
 	catalog_result = get_catalog(catalog_url.format(key))
 	print(catalog_result)
 	for catalog in catalog_result:
-		get_catalog_detail(books_url.format(catalog['id'], 1, 50, key), catalog['catalog'])
+		get_catalog_detail(books_url.format(catalog['id'], 1, 100, key), catalog)
